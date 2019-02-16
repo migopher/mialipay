024494c8975beaa9a6416b2203bb3732da14aca3
DESKTOP-V3UVV0N\xuzili <xuzili1994@aliyun.com>
2019-02-16 13:46:41 +0800

    更新

--- alipay.go
+++ alipay.go
@@ -1,13 +1,49 @@
-package go_alipay
+package alipay
+
+/**
+alipay
+ */
+
+const (
+	version            = "1.0"
+	format_json string = "JSON",
+)
 
 type Alipay struct {
+	//应用ID
+	AppId string
+	//支付宝公钥
+	AlipayPublicKey string
+	//商户私钥
+	MerchantPrivateKey string
+	//签名方式
+	SignType string
+	//支付宝网关
+	GatewayUrl string
+	//编码格式
+	//charset string
+	// 支付宝服务器主动通知商户服务器里指定的页面
+	NotifyUrl string
+	//异步通知地址,只有扫码支付预下单可用
+	ReturnUrl string
 
 }
 
-func init()  {
-	
+func NewAlipay(appid string, alipayPublicKey string, merchantPrivateKey string, signType string, gatewayUrl string, notifyUrl string, returnUrl string) *Alipay {
+	alipay := &Alipay{
+		appid,
+		alipayPublicKey,
+		merchantPrivateKey,
+		signType,
+		gatewayUrl,
+		notifyUrl,
+		returnUrl,
+	}
+	return alipay
 }
 
-func NewEngine()  {
-	
+func (a *Alipay) AlipayTradePay() *AlipayTradePay {
+	alipayTradePay := new(AlipayTradePay)
+	&alipayTradePay.Alipay = &a
+	return alipayTradePay
 }
---
+++ alipay_trade_pay.go
@@ -0,0 +1,53 @@
+package alipay
+
+import "time"
+
+const method_alipay_trade_pay string = "alipay.trade.pay"
+
+type AlipayTradePay struct {
+	method       string
+	Timestamp    string
+	format       string
+	charset		 string
+	Version      string
+	Format       string
+	AppAuthToken string
+	*Alipay
+	*BizContent
+}
+
+type BizContent struct {
+	OutTradeNo         string
+	Scene              string
+	AuthCode           string
+	ProductCode        string
+	Subject            string
+	BuyerId            string
+	SellerId           string
+	TotalAmount        string
+	TransCurrency      string
+	SettleCurrency     string
+	DiscountableAmount string
+	Body               string
+	GoodsDetail        string
+	OperatorId         string
+	StoreId            string
+	TerminalId         string
+	ExtendParams       string
+	TimeoutExpress     string
+	AuthConfirmMode    string
+	TerminalParams     string
+	PromoParams        string
+}
+
+func (atp *AlipayTradePay) init() {
+	atp.method=method_alipay_trade_pay
+	atp.format=format_json
+	atp.Timestamp=time.Now().Format("2006-01-02 15:04:05")
+	atp.Version=version
+}
+
+
+//func (atp *AlipayTradePay) setBizContent() *BizContent {
+//	return atp.BizContent
+//}
