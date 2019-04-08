d2c268b2c53893193b76be099619830f8a1df07c
ixuzl <863582605@qq.com>
2019-04-08 09:36:46 +0800

    update file

--- alipay_trade_pay.go
+++ alipay_trade_pay.go
@@ -1,4 +1,4 @@
-package alipay
+package mialipay
 
 import (
 	"encoding/json"
@@ -6,71 +6,77 @@ import (
 )
 
 const method_alipay_trade_pay string = "alipay.trade.pay"
-const alipay_trade_pay_url  = "?app_id=%v&method=%v&format=%v&charset=%v&sign_type=%v&sign=%v&timestamp=%v&version=%v&notify_url=%v&app_auth_token=%v&biz_content=%v"
+const alipay_common_parame = "app_id=%v&method=%v&format=%v&charset=%v&sign_type=%v&timestamp=%v&version=%v&notify_url=%v&app_auth_token=%v&biz_content=%v"
 
 type AlipayTradePay struct {
 	method       string
 	Timestamp    string
-	format       string
-	charset      string
 	Version      string
+	Sign         string
 	Format       string
 	AppAuthToken string
 	*Alipay
-	*BizContent
+	BizContent BizContent
 }
 
 type BizContent struct {
-	OutTradeNo         string `json:"out_trade_no"`
-	Scene              string `json:"scene"`
-	AuthCode           string `json:"auth_code"`
-	ProductCode        string `json:"product_code"`
-	Subject            string `json:"subject"`
-	BuyerId            string `json:"buyer_id"`
-	SellerId           string `json:"seller_id"`
-	TotalAmount        string `json:"total_amount"`
-	TransCurrency      string `json:"trans_currency"`
-	SettleCurrency     string `json:"settle_currency"`
-	DiscountableAmount string `json:"discountable_amount"`
-	Body               string `json:"body"`
-	GoodsDetail        *[]GoodsDetail `json:"goods_detail"`
-	OperatorId         string `json:"operator_id"`
-	StoreId            string `json:"store_id"`
-	TerminalId         string `json:"terminal_id"`
-	ExtendParams       *ExtendParams `json:"extend_params"`
-	TimeoutExpress     string `json:"timeout_express"`
-	AuthConfirmMode    string `json:"auth_confirm_mode"`
-	TerminalParams     string `json:"terminal_params"`
-	PromoParams        *PromoParams `json:"promo_params"`
+	OutTradeNo         string         `json:"out_trade_no,omitempty"`
+	Scene              string         `json:"scene,omitempty"`
+	AuthCode           string         `json:"auth_code,omitempty"`
+	ProductCode        string         `json:"product_code,omitempty"`
+	Subject            string         `json:"subject,omitempty"`
+	BuyerId            string         `json:"buyer_id,omitempty"`
+	SellerId           string         `json:"seller_id,omitempty"`
+	TotalAmount        string         `json:"total_amount,omitempty"`
+	TransCurrency      string         `json:"trans_currency,omitempty"`
+	SettleCurrency     string         `json:"settle_currency,omitempty"`
+	DiscountableAmount string         `json:"discountable_amount,omitempty"`
+	Body               string         `json:"body,omitempty"`
+	GoodsDetail        *[]GoodsDetail `json:"goods_detail,omitempty"`
+	OperatorId         string         `json:"operator_id,omitempty"`
+	StoreId            string         `json:"store_id,omitempty"`
+	TerminalId         string         `json:"terminal_id,omitempty"`
+	ExtendParams       *ExtendParams  `json:"extend_params,omitempty"`
+	TimeoutExpress     string         `json:"timeout_express,omitempty"`
+	AuthConfirmMode    string         `json:"auth_confirm_mode,omitempty"`
+	TerminalParams     string         `json:"terminal_params,omitempty"`
+	PromoParams        *PromoParams   `json:"promo_params,omitempty"`
 }
 type ExtendParams struct {
-	SysServiceProviderId string	 `json:"sys_service_provider_id"`
-	IndustryRefluxInfo   string	 `json:"industry_reflux_info"`
-	CardType             string	 `json:"card_type"`
+	SysServiceProviderId string `json:"sys_service_provider_id,omitempty"`
+	IndustryRefluxInfo   string `json:"industry_reflux_info,omitempty"`
+	CardType             string `json:"card_type,omitempty"`
 }
 
 type GoodsDetail struct {
-	GoodsId        string `json:"goods_id"`
-	GoodsName      string `json:"goods_name"`
-	Quantity       string `json:"quantity"`
-	Price          string `json:"price"`
-	GoodsCategory  string `json:"goods_category"`
-	CategoriesTree string `json:"categories_tree"`
-	Body           string `json:"body"`
-	ShowUrl        string `json:"show_url"`
+	GoodsId        string `json:"goods_id,omitempty"`
+	GoodsName      string `json:"goods_name,omitempty"`
+	Quantity       string `json:"quantity,omitempty"`
+	Price          string `json:"price,omitempty"`
+	GoodsCategory  string `json:"goods_category,omitempty"`
+	CategoriesTree string `json:"categories_tree,omitempty"`
+	Body           string `json:"body,omitempty"`
+	ShowUrl        string `json:"show_url,omitempty"`
 }
 
 type PromoParams struct {
-	ActualOrderTime string
+	ActualOrderTime string `json:"actual_order_time,omitempty"`
+}
+
+func (atp *AlipayTradePay) BuildUrl() string {
+	biz,_:=atp.BizContent.ToJson()
+	strUrl := fmt.Sprintf(alipay_common_parame, atp.AppId, method_alipay_trade_pay, atp.Format, atp.Charset, atp.SignType, atp.Timestamp, atp.Version, atp.NotifyUrl, atp.AppAuthToken, biz)
+	ccc := BuildSign(strUrl, atp.MerchantPrivateKey)
+	return atp.GatewayUrl + "?" + strUrl + "&sign=" + ccc
 }
 
 func (atp *AlipayTradePay) SetBizContent(bizContent BizContent) {
-	atp.BizContent = &bizContent
+	atp.BizContent = bizContent
 }
 
-//func (bc *BizContent) setPromoParams(promoParams PromoParams) {
-//	*bc.PromoParams = promoParams
-//}
+func (bc *BizContent) setPromoParams(promoParams PromoParams) {
+	*bc.PromoParams = promoParams
+}
 
 func (bc *BizContent) SetGoodsDetail(goodsDetail []GoodsDetail) {
 	bc.GoodsDetail = &goodsDetail
@@ -79,16 +85,14 @@ func (bc *BizContent) SetGoodsDetail(goodsDetail []GoodsDetail) {
 func (bc *BizContent) SetExtendParams(extendParams ExtendParams) {
 	*bc.ExtendParams = extendParams
 }
-func (bc *BizContent) ToJson()(string,error) {
+func (bc *BizContent) ToJson() (string, error) {
 	byteJson, err := json.Marshal(bc)
 	if err != nil {
-		return "",err
+		return "", err
 	}
-	return string(byteJson),nil
+	return string(byteJson), nil
 }
 
-func (atp *AlipayTradePay)ToUrl() interface{} {
-	request:=atp.method+alipay_trade_pay_url
-	//url:=fmt.Printf(request,atp.AppId,atp.method,atp.format,atp.charset,atp.SignType,atp.)
-	return nil
-}
+//func ExecRequest()  {
+//
+//}
